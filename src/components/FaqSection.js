8945ccba327d1399e305c0dcc8ed0906568aac8a
Shahzod <shahzodmaxammatov288@gmail.com>
2022-10-29 09:15:42 +0500

    FAQ Section html completed

---
+++ src/components/FaqSection.js
@@ -0,0 +1,53 @@
+import React from "react";
+
+const FaqSection = () => {
+  return (
+    <div className="faq">
+      <h2>
+        Any Questions <span>FAQ</span>
+      </h2>
+      <div className="question">
+        <h2>Can I learn programming from 0 your platform?</h2>
+        <div className="answer">
+          <p>Lorem ipsum dolor sit amet...</p>
+          <p>
+            Lorem ipsum dolor sit amet consectetur adipisicing elit. Aliquam
+            earum, id pariatur omnis eos consequatur?
+          </p>
+        </div>
+      </div>
+      <div className="question">
+        <h2>How much do I have to pay per months?</h2>
+        <div className="answer">
+          <p>Lorem ipsum dolor sit amet...</p>
+          <p>
+            Lorem ipsum dolor sit amet consectetur adipisicing elit. Aliquam
+            earum, id pariatur omnis eos consequatur?
+          </p>
+        </div>
+      </div>
+      <div className="question">
+        <h2>Is it possible to pay in full in one go?</h2>
+        <div className="answer">
+          <p>Lorem ipsum dolor sit amet...</p>
+          <p>
+            Lorem ipsum dolor sit amet consectetur adipisicing elit. Aliquam
+            earum, id pariatur omnis eos consequatur?
+          </p>
+        </div>
+      </div>
+      <div className="question">
+        <h2>If I buy the course, can my brother and I also study</h2>
+        <div className="answer">
+          <p>Lorem ipsum dolor sit amet...</p>
+          <p>
+            Lorem ipsum dolor sit amet consectetur adipisicing elit. Aliquam
+            earum, id pariatur omnis eos consequatur?
+          </p>
+        </div>
+      </div>
+    </div>
+  );
+};
+
+export default FaqSection;
--- src/pages/AboutUs.js
+++ src/pages/AboutUs.js
@@ -2,12 +2,14 @@ import React from "react";
 //! import components
 import AboutSection from "../components/AboutSection";
 import BenefitsSection from "../components/BenefitsSection";
+import FaqSection from "../components/FaqSection";
 
 const AboutUs = () => {
   return (
     <>
       <AboutSection />
       <BenefitsSection />
+      <FaqSection />
     </>
   );
 };
