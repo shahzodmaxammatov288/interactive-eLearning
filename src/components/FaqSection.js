182be3d5c5f24ae6f5e361e754edb4cc5bd37cda
Shahzod <shahzodmaxammatov288@gmail.com>
2022-10-29 23:10:43 +0500

    FAQ section styling static completed

--- src/components/FaqSection.js
+++ src/components/FaqSection.js
@@ -1,13 +1,16 @@
 import React from "react";
-
+//! import styled
+import styled from "styled-components";
+//! styles
+import { About } from "../styles";
 const FaqSection = () => {
   return (
-    <div className="faq">
+    <Faq>
       <h2>
         Any Questions <span>FAQ</span>
       </h2>
       <div className="question">
-        <h2>Can I learn programming from 0 your platform?</h2>
+        <h4>Can I learn programming from 0 your platform?</h4>
         <div className="answer">
           <p>Lorem ipsum dolor sit amet...</p>
           <p>
@@ -15,9 +18,10 @@ const FaqSection = () => {
             earum, id pariatur omnis eos consequatur?
           </p>
         </div>
+        <div className="faq-line"></div>
       </div>
       <div className="question">
-        <h2>How much do I have to pay per months?</h2>
+        <h4>How much do I have to pay per months?</h4>
         <div className="answer">
           <p>Lorem ipsum dolor sit amet...</p>
           <p>
@@ -25,9 +29,10 @@ const FaqSection = () => {
             earum, id pariatur omnis eos consequatur?
           </p>
         </div>
+        <div className="faq-line"></div>
       </div>
       <div className="question">
-        <h2>Is it possible to pay in full in one go?</h2>
+        <h4>Is it possible to pay in full in one go?</h4>
         <div className="answer">
           <p>Lorem ipsum dolor sit amet...</p>
           <p>
@@ -35,9 +40,10 @@ const FaqSection = () => {
             earum, id pariatur omnis eos consequatur?
           </p>
         </div>
+        <div className="faq-line"></div>
       </div>
       <div className="question">
-        <h2>If I buy the course, can my brother and I also study</h2>
+        <h4>If I buy the course, can my brother and I also study</h4>
         <div className="answer">
           <p>Lorem ipsum dolor sit amet...</p>
           <p>
@@ -45,9 +51,35 @@ const FaqSection = () => {
             earum, id pariatur omnis eos consequatur?
           </p>
         </div>
+        <div className="faq-line"></div>
       </div>
-    </div>
+    </Faq>
   );
 };
+
+//! Styles
+const Faq = styled(About)`
+  display: block;
+  h2 {
+    padding-bottom: 2rem;
+    font-weight: lighter;
+  }
+  .question {
+    padding: 3rem 0;
+    cursor: pointer;
+  }
+  .answer {
+    padding: 2rem 0;
+    p {
+      padding: 1rem 0;
+    }
+  }
+  .faq-line {
+    background-color: #ccc;
+    height: 0.2rem;
+    width: 100%;
+    margin: 2rem 0;
+  }
+`;
 
 export default FaqSection;
