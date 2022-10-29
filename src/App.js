ee24de180ef09920a3f5692d9502e821cd812791
Shahzod <shahzodmaxammatov288@gmail.com>
2022-10-29 10:24:54 +0500

    Styled Component Intro

--- src/App.js
+++ src/App.js
@@ -1,11 +1,13 @@
 import React from "react";
+//! Global style
+import GlobalStyle from "./components/GlobalStyle";
 //! import pages
 import AboutUs from "./pages/AboutUs";
 
 function App() {
   return (
     <div className="App">
-      <h1>eLearning</h1>
+      <GlobalStyle />
       <AboutUs />
     </div>
   );
--- src/components/AboutSection.js
+++ src/components/AboutSection.js
@@ -1,31 +1,63 @@
 import React from "react";
 //! import images
 import homeIntro1 from "../images/home_intro1.jpg";
+//! import styled
+import styled from "styled-components";
 
 const AboutSection = () => {
   return (
-    <div className="description">
-      <div className="title">
-        <div className="hide">
-          <h2>Be educated so that</h2>
+    <About>
+      <Description>
+        <div className="title">
+          <Hide>
+            <h2>Be educated so that</h2>
+          </Hide>
+          <Hide>
+            <h2>
+              you <span>can changes</span>
+            </h2>
+          </Hide>
+          <Hide>
+            <h2>the world.</h2>
+          </Hide>
+          <p>
+            An educated mind can teach many. An educated mind is better than
+            empty one.
+          </p>
+          <button>About us</button>
         </div>
-        <div className="hide">
-          <h2>you can changes</h2>
-        </div>
-        <div className="hide">
-          <h2>the world.</h2>
-        </div>
-        <p>
-          An educated mind can teach many. An educated mind is better than empty
-          one.
-        </p>
-        <button>About us</button>
-      </div>
-      <div className="image">
-        <img src={homeIntro1} alt="Boy and Girl reading a book" width={200} />
-      </div>
-    </div>
+      </Description>
+      <Image>
+        <img src={homeIntro1} alt="Boy and Girl reading a book" />
+      </Image>
+    </About>
   );
 };
+
+const About = styled.div`
+  display: flex;
+  justify-content: space-around;
+  align-items: center;
+  padding: 5rem 10rem;
+`;
+
+const Description = styled.div`
+  flex: 1;
+  padding-right: 5rem;
+`;
+
+const Image = styled.div`
+  flex: 1;
+  overflow: hidden;
+  img {
+    width: 400px;
+    /* height: 80vh; */
+    object-fit: cover;
+  }
+`;
+
+const Hide = styled.div`
+  overflow: hidden;
+`;
 
 export default AboutSection;
