72b66b000655b6eaeec35e7a503bc9d6accaf94e
Shahzod <shahzodmaxammatov288@gmail.com>
2022-10-30 11:26:24 +0500

    Courses html and Styling completed

--- src/pages/Courses.js
+++ src/pages/Courses.js
@@ -1,11 +1,58 @@
 import React from "react";
+import styled from "styled-components";
+import { Link } from "react-router-dom";
+import react from "../images/react.png";
+import soon from "../images/soon.png";
+import js from "../images/js.png";
 
 const Courses = () => {
   return (
-    <div>
-      <h1>Courses</h1>
-    </div>
+    <StyledCourses>
+      <Course>
+        <h2>Interaktiv React va Redux</h2>
+        <div className="line"></div>
+        <Link>
+          <img src={react} alt="Interaktiv React va Redux" />
+        </Link>
+      </Course>
+      <Course>
+        <h2>Tez kunda...😯</h2>
+        <div className="line"></div>
+        <Link>
+          <img src={soon} alt="Tez kunda..." />
+        </Link>
+      </Course>
+      <Course>
+        <h2>Interaktiv JavaScript</h2>
+        <div className="line"></div>
+        <Link>
+          <img src={js} alt="Interaktiv JavaScript" />
+        </Link>
+      </Course>
+    </StyledCourses>
   );
 };
+
+const StyledCourses = styled.div`
+  min-height: 100vh;
+  padding: 5rem 10rem;
+  h2 {
+    padding: 1rem 0;
+  }
+`;
+
+const Course = styled.div`
+  padding-bottom: 10rem;
+  .line {
+    height: 0.5rem;
+    background: #ccc;
+    margin-bottom: 3rem;
+  }
+  img {
+    width: 100%;
+    height: 70vh;
+    object-fit: cover;
+  }
+`;
 
 export default Courses;
